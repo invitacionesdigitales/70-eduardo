0e21178416acd0ece0e8da6e217c765eb293d534
invitacionesdigitales <111189010+invitacionesdigitales@users.noreply.github.com>
2023-05-28 16:01:09 -0300

    8

--- main.js
+++ main.js
@@ -6,19 +6,7 @@
     $finalMessage = document.querySelector('.final-sms');
     let boton = document.querySelector("#btn")
 
-    boton.addEventListener("click", () => {
-
-        Swal.fire({
-          title: '<strong>VER LA UBICACION <u></u></strong>',
-          icon: 'success',
-          html:
-            'INGRESA' +
-            '<iframe src="https://www.google.com/ma3645062!2d-58.81571198543341!3d-34.63977998045015!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x95bc941338df622b%3A0xdba6b2bc21820454!2sLa%20Piedad%201450%2C%20B1738GTD%20La%20Reja%2C%20Provincia%20de%20Buenos%20Aires!5e0!3m2!1ses!2sar!4v1675255606944!5m2!1ses!2sar" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>' +
-            'Podes ampliar en mapa',
-            
-            
-        })
-      });
+ 
 
 
    
